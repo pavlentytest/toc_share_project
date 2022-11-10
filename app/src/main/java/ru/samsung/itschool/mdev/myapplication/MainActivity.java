4412f26589436c7cc96ddce6478d50c05e4c0aff
nevasik <nevasik@mail.ru>
2022-11-10 20:44:19 +0300

    added a button with a counter and solved the problem with screen rotation

--- app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
+++ app/src/main/java/ru/samsung/itschool/mdev/myapplication/MainActivity.java
@@ -1,14 +1,41 @@
 package ru.samsung.itschool.mdev.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.PoplaukhinId);
+        textView = findViewById(R.id.textView2);
+        button.setOnClickListener(view -> {
+            counter++;
+            String result = "Вы тыкнули на мою кнопочку " + Integer.toString(counter);
+            textView.setText(result);
+                });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("value",counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("value");
+        textView.setText(Integer.toString(counter));
     }
 }
